5704c566e72340dd7ef0ac8edab00b4f413de4d0
Yarilo <contact@yarilovillanueva.com>
2020-11-07 13:24:21 +0100

    Missing file

---
+++ src/Navigation.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<main>
+	<ul class='navigation-drawer'>
+		<li><a href="#about">About</a></li>
+		<li><a href="#portfolio">Porfolio</a></li>
+		<li><a href="#contact">Contact</a></li>
+	</ul>
+
+	
+</main>
+
+<style>
+	ul {
+		list-style: none;
+	}
+	li {
+		margin-top: 50px;
+	}
+	a {
+		color: black;
+	}
+	.navigation-drawer {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+</style>
