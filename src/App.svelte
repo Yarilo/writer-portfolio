503d13a945e8a1da2d914f191805f0d227687625
Yarilo <contact@yarilovillanueva.com>
2020-11-07 13:24:07 +0100

    Move navigation drawer to its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,10 @@
 <script>
-	
+	import Navigation from './Navigation.svelte';
+	import About from "./About.svelte";
 </script>
 
 <main>
-	<ul class='navigation-drawer'>
-		<li>About</li>
-		<li>Portfolio</li>
-		<li>Contact</li>
-	</ul>
+	<Navigation />
 	<div class='title'>
 		<h1>SV</h1>
 		<h2>Screenwriter</h2>
@@ -16,7 +13,6 @@
 </main>
 
 <style>
-
 	@import url('https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap');
 	main {
 		width: 100%;
@@ -27,33 +23,15 @@
 		font-family: 'Courier Prime', monospace;
 	}
 
-	ul {
-		list-style: none;
-	}
-	li {
-		margin-top: 50px;
-	}
-	.navigation-drawer {
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
-
 	.title {
 		text-align: center;
 		flex: 2;
 	}
+
 	h1 {
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 	
 </style>
