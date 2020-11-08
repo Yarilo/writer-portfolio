a59f457c837d8dd567e2fee5c791dff69714d63d
Yarilo <contact@yarilovillanueva.com>
2020-11-08 14:38:26 +0100

    Working on the profile

--- src/Portfolio.svelte
+++ src/Portfolio.svelte
@@ -1,7 +1,19 @@
 <script>
+// @TODO: On click work, show its text
+const works = [
+    {title: 'The bla & the ble 1'}, 
+    {title: 'The bla & the ble 2'}
+]
 </script>
-
-<p>Tol portoflio</p>
 
 <style>
 </style>
+
+<div>
+    <h3>Some of my works:</h3>
+    <ul>
+        {#each works as work} 
+        <li>{work.title}</li>
+        {/each}
+    </ul>
+</div>
