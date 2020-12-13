a2950d0d24ae4cbbcccbef2194835efdf134fdb2
Yarilo <contact@yarilovillanueva.com>
2020-12-13 18:53:51 +0100

    About section v1

--- src/About.svelte
+++ src/About.svelte
@@ -3,8 +3,9 @@
 </script>
     <h2>About</h2>
     <p>I'm Samuel Villanueva, a Screenwriter from Canary Islands.</p>
-    <p>I enjoy creating stories and conducting narratives, focusing on the dark fantasy & psychological terror genres</p>
-
+    <p>I enjoy creating stories and conducting narratives, focusing on the dark fantasy & psychological terror genres.</p>
+    <p>Besides of storytelling, I enjoy videogames, books and of course, films. </p>
+    <p>Currently working on a short film to be filmed in Las Palmas de Gran Canaria in 2021.</p>
 <style>
 
 	
