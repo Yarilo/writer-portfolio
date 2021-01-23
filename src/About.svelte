f1de2f607bf3106fc866b3367384066107f60afd
Yarilo <contact@yarilovillanueva.com>
2021-01-23 18:22:17 +0100

    Adjusting markup in about

--- src/About.svelte
+++ src/About.svelte
@@ -5,11 +5,11 @@
     <p>Soy Samuel Villanueva Hernández, escritor y guionista especializado en lo audiovisual.</p>
     <p>Tengo un título de Comunicaciones Audiovisuales de la Universidad de Málaga y
     <p>un máster de Creación de Guiones Audiovisuales de la Universidad Internacional de la Rioja.</p>
-    <br/><p></p>
+    <br/><p/>
     <p>Me encanta escribir historias y manejar narrativas, siendo mi foco la fantasía oscura y el horror psicológico.
     <p>
     Además del arte de contar historias, me gustan los videojuegos, los libros y por supuesto, las películas.</p> 
-    <br/><p></p>
+    <br/></p>
     <p>Actualmente estoy trabajando en un cortometraje que se filmara en Las Palmas en 2021.</p>
 <style>
 
