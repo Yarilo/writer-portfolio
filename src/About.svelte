1ce5438fecca86b4095832d47c25dd82932b7c08
Yarilo <contact@yarilovillanueva.com>
2021-02-06 11:37:06 +0100

    Padding on about description for better legibility

--- src/About.svelte
+++ src/About.svelte
@@ -3,17 +3,22 @@
 </script>
     <div class='about'>
         <h2>Sobre mi</h2>
-        <p>Soy Samuel Villanueva Hernández, escritor y guionista especializado en lo audiovisual.</p>
-        <p>Tengo un título de Comunicaciones Audiovisuales de la Universidad de Málaga y
-        <p>un máster de Creación de Guiones Audiovisuales de la Universidad Internacional de la Rioja.</p>
-        <br/><p></p>
-        <p>Me encanta escribir historias y manejar narrativas, siendo mi foco la fantasía oscura y el horror psicológico.
-        <p>
-        Además del arte de contar historias, me gustan los videojuegos, los libros y por supuesto, las películas.</p> 
-        <br/><p></p>
-        <p>Actualmente estoy trabajando en un cortometraje que se filmara en Las Palmas en 2021.</p>
+        <div class='description'>
+            <p>Soy Samuel Villanueva Hernández, escritor y guionista especializado en lo audiovisual.</p>
+            <p>Tengo un título de Comunicaciones Audiovisuales de la Universidad de Málaga y
+            <p>un máster de Creación de Guiones Audiovisuales de la Universidad Internacional de la Rioja.</p>
+            <br/><p></p>
+            <p>Me encanta escribir historias y manejar narrativas, siendo mi foco la fantasía oscura y el horror psicológico.
+            <p>
+            Además del arte de contar historias, me gustan los videojuegos, los libros y por supuesto, las películas.</p> 
+            <br/><p></p>
+            <p>Actualmente estoy trabajando en un cortometraje que se filmara en Las Palmas en 2021.</p>
+        </div>
     </div>
 <style>
+  .description {
+      padding-top: 30px;
+    }
 @media (max-width:800px)  { 
     .about{
         width: 90%;
