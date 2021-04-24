9cf370129951dc2f9077635c634a87a0f9f2b739
Yarilo <contact@yarilovillanueva.com>
2021-04-24 20:23:53 +0200

    Modify about section as per writer's petition
    Additionally applying some adjustments in the style, since now there is more text and we need to justify it properly.

--- src/About.svelte
+++ src/About.svelte
@@ -4,24 +4,36 @@
     <div class='about'>
         <h2>Sobre mi</h2>
         <div class='description'>
-            <p>Soy Samuel Villanueva Hernández, escritor y guionista especializado en lo audiovisual.</p>
-            <p>Tengo un título de Comunicaciones Audiovisuales de la Universidad de Málaga y
-            <p>un máster de Creación de Guiones Audiovisuales de la Universidad Internacional de la Rioja.</p>
+            <p>Soy Samuel Villanueva Hernández, escritor y guionista especializado en lo audiovisual. Tengo un título de Comunicaciones Audiovisuales de la Universidad de Málaga y un máster de Creación de Guiones Audiovisuales de la Universidad Internacional de la Rioja.</p>
+            <br/>
+            <p>Me encanta escribir historias y manejar narrativas, siendo mi foco la fantasía oscura y el horror psicológico. Estos géneros siempre me han fascinado debido a que suelen explorar temas complejos desde una perspectiva única y es algo que he tratado de imitar en mis historias.<p>
+            <br /><p></p>
+            <p>Además del arte de contar historias, me gustan los videojuegos, los libros y por supuesto, las películas y las series. Me suelen atraer historias ficticias lo más alejadas de la realidad, que muestran mundos o situaciones imposibles que a su vez exploren temas importantes para nuestra sociedad. Es una contradicción que adoro, por lo que tengo especial interés en la animación y los videojuegos ya que es donde creo que hay más potencial para este tipo de historias.</p> 
             <br/><p></p>
-            <p>Me encanta escribir historias y manejar narrativas, siendo mi foco la fantasía oscura y el horror psicológico.
-            <p>
-            Además del arte de contar historias, me gustan los videojuegos, los libros y por supuesto, las películas.</p> 
-            <br/><p></p>
+            <p>Lo que busco con mi trabajo es entretener a la gente y aportar algo diferente o al menos, personal, al mundo</p>
             <p>Actualmente estoy trabajando en un cortometraje que se filmara en Las Palmas en 2021.</p>
         </div>
     </div>
 <style>
+
+    .about {
+        width: 50%;
+    }
+
   .description {
       padding-top: 30px;
+      line-height: 1.75;
+      text-align: justify;
     }
 @media (max-width:800px)  { 
     .about{
-        width: 90%;
+        height: 95%;
+        width: 94%;
+    }
+    .description {
+        padding-left: 5px;
+        padding-right: 5px;
+        text-align: justify;
     }
 }
 </style>
